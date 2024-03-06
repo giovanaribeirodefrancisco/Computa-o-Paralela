ea55799a16d5f178a591f7e3ca65d68edeb1b564
Cloud User <ec2-user@ip-172-31-29-206.ec2.internal>
2024-03-06 20:55:07 +0000

    Lab02

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -1,2 +1,36 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int matriz[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
+int vetor[3] = {1,2,3};
+int resultado[3];
+
+void* mult(void* num){
+  long nume = (long) num;
+  int i,j;
+  resultado[nume]=0;
+  for(i=0;i<3;i++)
+    resultado[nume]+= matriz[nume][i]*vetor[i];
+  
+}
+
+int main(void) {
+  int linhas = sizeof(matriz)/sizeof(matriz[0]);
+  int colunas = sizeof(matriz[0])/sizeof(int);
+  pthread_t* thread_rope=malloc(linhas * sizeof(pthread_t));
+
+  for(long linha = 0; linha<linhas; linha++){
+    pthread_create(&thread_rope[linha], NULL, mult, (void*) linha);
+  }
+
+  for(int i=0; i<linhas; i++){
+    pthread_join(thread_rope[i], NULL);
+  }
+  for(int k = 0; k < linhas; k++) {
+    printf("%d ", resultado[k]);
+  }
+  printf("\n");
+  
+  return 0;
+}
