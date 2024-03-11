2bbe9765fd52aa4ae0f94d47cae421eae4f725be
Cloud User <ec2-user@ip-172-31-34-51.ec2.internal>
2024-03-11 19:21:51 +0000

    Letíca código

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -2,23 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int matriz[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
-int vetor[3] = {1,3,3};
+int matriz[3][3] = {{1,2,3},{3,2,1},{2,2,2}};
+int vetor[3] = {3,2,1};
 int resultado[3];
 
 void* mult(void* num){
-  long nume = (long) num;
-  int i,j;
+  long nume = (long) num;;
   resultado[nume]=0;
-  for(i=0;i<3;i++)
+  for(int i=0;i<3;i++)
     resultado[nume]+= matriz[nume][i]*vetor[i];
   return NULL;
+}
+
+void imprimir(int colunas, int linhas, int v[linhas][colunas]){
+  for(int i = 0; i < linhas; i++){
+    for(int y = 0; y < colunas; y++){
+      printf("%d ", v[i][y]);
+    }
+   printf("\n");
+ }
+
 }
 
 int main(void) {
   int linhas = sizeof(matriz)/sizeof(matriz[0]);
   int colunas = sizeof(matriz[0])/sizeof(int);
   pthread_t* thread_rope=malloc(linhas * sizeof(pthread_t));
+
+ printf("Matriz:\n");
+ imprimir(colunas, linhas, matriz);
+
+ printf("Vetor:\n");
+ for(int i = 0; i<colunas; i++){
+  printf("%d\n", vetor[i]);
+ }
 
   for(long linha = 0; linha<linhas; linha++){
     pthread_create(&thread_rope[linha], NULL, mult, (void*) linha);
@@ -27,8 +44,10 @@ int main(void) {
   for(int i=0; i<linhas; i++){
     pthread_join(thread_rope[i], NULL);
   }
+
+  printf("Resultado:\n");
   for(int k = 0; k < linhas; k++) {
-    printf("%d ", resultado[k]);
+    printf("%d\n", resultado[k]);
   }
   printf("\n");
   free(thread_rope); 
