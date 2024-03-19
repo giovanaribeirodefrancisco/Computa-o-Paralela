950f0d8a8d33a0b0a6e021942198bd70ec0bd64c
Giovana Ribeiro de Francisco <161640729+giovanaribeirodefrancisco@users.noreply.github.com>
2024-03-19 19:12:36 -0300

    Update lab02b.c

--- lab02b/lab02b.c
+++ lab02b/lab02b.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 100
+#define N 10000
 
-int matriz[N][N];
-int vetor[N];
-int resultado[N];
+int matriz[N][100];
+int vetor[100];
+int resultado[100];
+int contador_thread;
 
-void* mult(void* num){
-  long nume = (long) num;;
-  resultado[nume]=0;
-  for(int i=0;i<3;i++)
-    resultado[nume]+= matriz[nume][i]*vetor[i];
+void* mult(void* rank){
+  long meurank = (long) rank;
+  int local = N/contador_thread;
+  int primeira = meurank*local;
+  int ultima = (meurank+1)*local-1;
+  for(int i = primeira; i <= ultima; i++){
+    resultado[i] = 0;
+    for(int j=0;j<100;j++)
+      resultado[i]+= matriz[i][j]*vetor[j];
+  }
   return NULL;
 }
 
@@ -34,35 +40,41 @@ void preencher(int colunas, int linhas, int v[linhas][colunas]){
   }
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("Uso: %s <numero_de_threads>\n", argv[0]);
+    return 1;
+  }
   int linhas = sizeof(matriz)/sizeof(matriz[0]);
   int colunas = sizeof(matriz[0])/sizeof(int);
-  pthread_t* thread_rope=malloc(linhas * sizeof(pthread_t));
-preencher(colunas,linhas, matriz);
+  contador_thread = strtol(argv[1], NULL, 10);
   
- printf("Matriz:\n");
- imprimir(colunas, linhas, matriz);
+  pthread_t* thread_rope=(pthread_t *)malloc(contador_thread * sizeof(pthread_t));
+preencher(colunas,linhas, matriz);
 
-  for(int a = 0; a < linhas; a++){
+ /*printf("Matriz:\n");
+ imprimir(colunas, linhas, matriz);*/
+
+  for(int a = 0; a < colunas; a++){
     vetor[a] = rand() % 10;
   }
- printf("Vetor:\n");
+ /*printf("Vetor:\n");
  for(int i = 0; i<colunas; i++){
   printf("%d\n", vetor[i]);
- }
+ }*/
 
-  for(long linha = 0; linha<linhas; linha++){
-    pthread_create(&thread_rope[linha], NULL, mult, (void*) linha);
+  for(long thread = 0; thread<contador_thread; thread++){
+    pthread_create(&thread_rope[thread], NULL, mult, (void*) thread);
   }
 
-  for(int i=0; i<linhas; i++){
+  for(int i=0; i<contador_thread; i++){
     pthread_join(thread_rope[i], NULL);
   }
 
-  printf("Resultado:\n");
+  /*printf("Resultado:\n");
   for(int k = 0; k < linhas; k++) {
     printf("%d\n", resultado[k]);
-  }
+  }*/
   printf("\n");
   free(thread_rope); 
   return 0;
