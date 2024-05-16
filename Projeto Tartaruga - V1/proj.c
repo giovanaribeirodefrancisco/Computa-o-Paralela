3b39c189fad519dc95e7d9656027f059a4fcb4ad
LeticiaMoraesG <130491724+LeticiaMoraesG@users.noreply.github.com>
2024-05-16 08:42:53 -0300

    15000 casas proj.c

--- Projeto Tartaruga - V1/proj.c
+++ Projeto Tartaruga - V1/proj.c
@@ -1,60 +1,65 @@
-#include <stdio.h>
+include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
 #include <mpfr.h>
 
 void fatorial(int n, mpfr_t* vet) {
-	mpfr_init2(vet[0], 5000); // Inicializa com precisão de 5000 bits
-	mpfr_set_d(vet[0], 1.0, MPFR_RNDU);
+        mpfr_init2(vet[0], 50000); // Inicializa com precisão de 64 bits
+        mpfr_set_d(vet[0], 1.0, MPFR_RNDU);
 
-	mpfr_init2(vet[1], 5000); // Inicializa com precisão de 5000 bits
-	mpfr_set_d(vet[1], 1.0, MPFR_RNDU);
+        mpfr_init2(vet[1], 50000); // Inicializa com precisão de 64 bits
+        mpfr_set_d(vet[1], 1.0, MPFR_RNDU);
 
-	for (int i = 2; i < n; ++i) {
-		mpfr_init2(vet[i], 5000); // Inicializa com precisão de 5000 bits
-		mpfr_mul_si(vet[i], vet[i - 1], i, MPFR_RNDU);
-	}
+        for (int i = 2; i < n; ++i) {
+                mpfr_init2(vet[i], 50000); // Inicializa com precisão de 64 bits
+                mpfr_mul_si(vet[i], vet[i - 1], i, MPFR_RNDU);
+        }
 }
 
-void soma(int n, mpfr_t* vet, mpfr_t* globalP) {
-	int posicao = omp_get_thread_num();
-		int threads = omp_get_num_threads();
+void soma(int n, mpfr_t* vet, mpfr_t* globalPointer) {
+        int posicao = omp_get_thread_num();
+  int threads = omp_get_num_threads();
 
-		int parte = n/threads;
-		int inicio = parte * posicao;
-		int fim = inicio + parte - 1;
+                int parte = n/threads;
+                int inicio = parte * posicao;
+                int fim = inicio + parte - 1;
 
-	mpfr_t parcial;
-	mpfr_init2(parcial, 5000); //Inicializa com precisão 5000
-	mpfr_set_d(partial, 0.0, MPFR_RNDU);
+        mpfr_t parcial_local, resultdivisao; 
+        mpfr_init2(parcial_local, 50000); // Inicializa com precisão de 64 bits
+        mpfr_set_d(parcial_local, 0.0, MPFR_RNDU);
+        mpfr_init2(resultdivisao, 50000);
+        mpfr_set_d(resultdivisao, 1.0, MPFR_RNDU);
 
-	for (int i = inicio; i <= fim; i++){
-		mpfr_div(vet[i], vet[0], vet[i], MPFR_RNDU);
-		mofr_add(partial, partial, vet[i], MPFR_RNDU);
-	}
-# pragma omp critical
-	mpfr_add(*globalP, *globalP, parcial, MPFR_RNDU);
+    for (int i = inicio; i <= fim; i++) {
+        mpfr_div(resultdivisao, vet[0], vet[i], MPFR_RNDU);
+        mpfr_add(parcial_local, parcial_local, resultdivisao, MPFR_RNDU);
+    }
+#pragma omp critical
+        mpfr_add(*globalPointer, *globalPointer, parcial_local, MPFR_RNDU);
 
-	mpfr_clear(parcial);
+
+mpfr_clear(parcial_local);
 }
 
 double calcular_e_paralelo(int n_termos, int Threads) {
 
-	mpfr_t* vet = (mpfr_t*)malloc(n_termos * sizeof(mpfr_t));
-	fatorial(n_termos, vet);
 
-	mpfr_t global;
-	mpfr_init2(global, 5000); // Inicializa com precisão de 5000 bits
-	mpfr_set_d(global, 0.0, MPFR_RNDU);
-# 		pragma omp parallel num_threads(Threads)
-	soma(n_termos, vet, &global);
+        mpfr_t* vet = (mpfr_t*)malloc(n_termos * sizeof(mpfr_t));
+        fatorial(n_termos, vet);
 
-	printf("Resultado da aproximação de Euler: ");
-	mpfr_out_str(stdout, 10, 0, global, MPFR_RNDU);
-	printf("\n");
+        mpfr_t global;
+        mpfr_init2(global, 50000); // Inicializa com precisão de 64 bits
+        mpfr_set_d(global, 0.0, MPFR_RNDU);
+#               pragma omp parallel num_threads(Threads)
+        soma(n_termos, vet, &global);
+
+        printf("Resultado da aproximação de Euler: ");
+        mpfr_out_str(stdout, 10, 0, global, MPFR_RNDU);
+        printf("\n");
 
 	
 	mpfr_clear(global);
+	mpfr_clear(resultdivisao);
 	for (int i = 0; i < n_termos; i++) {
 		mpfr_clear(vet[i]);
 	}
@@ -72,7 +77,7 @@ int main(int argc, char* argv[]) {
 
 	int nThreads = strtol(argv[1], NULL, 10);
 
-	int n_termos = 10000000;
+	int n_termos = 1000000;
 	calcular_e_paralelo(n_termos,nThreads);
 
 	printf("Valor de e (paralelo): %.100f\n", e_paralelo);
